fc168d8e869d1f283ea8ff0b25edd96841965e2b
Deo silaen <deokeldisilaen@gmail.com>
2025-03-15 16:35:06 +0700

    add UI task and Progress

--- Assets/Scripts/Mechanics/Interactions/Container.cs
+++ Assets/Scripts/Mechanics/Interactions/Container.cs
@@ -9,24 +9,38 @@ public class Container : Interactable
     public GameObject fullContainer;
     public List<GameObject> storedItems = new List<GameObject>();
     public int maxCapacity = 1;
+    TaskManager taskManager;
 
-    public override string Description(){
-        if(PlayerInteractions.heldItem != null) {
+    void Start()
+    {
+        taskManager = TaskManager.Instance;
+    }
+
+    public override string Description()
+    {
+        if (PlayerInteractions.heldItem != null)
+        {
             return "Press {E} to interact.";
-        }else{
+        }
+        else
+        {
             return " ";
         }
     }
 
 
-    public override void Interact(){
-        if(PlayerInteractions.heldItem != null){
+    public override void Interact()
+    {
+        if (PlayerInteractions.heldItem != null)
+        {
             Collecting();
         }
     }
 
-    void Collecting(){
-        if (storedItems.Count < maxCapacity){
+    void Collecting()
+    {
+        if (storedItems.Count < maxCapacity)
+        {
             PlayerInteractions.heldItem.transform.SetParent(containerSlot);
             PlayerInteractions.heldItem.transform.localPosition = new Vector3(0, storedItems.Count * 0.2f, 0); // Menyusun item ke atas
             storedItems.Add(PlayerInteractions.heldItem.gameObject);
@@ -34,12 +48,33 @@ public class Container : Interactable
         }
     }
 
-    void Update(){
-        if(storedItems.Count == maxCapacity){
+    void Update()
+    {
+        if (storedItems.Count == maxCapacity)
+        {
             emptyContainer.SetActive(false);
             Destroy(emptyContainer);
             fullContainer.SetActive(true);
             Debug.Log("Penuhh");
+
+            if (taskManager != null)
+            {
+                Task task = FindTaskByName("Masukkan Item Ke Container1");
+                if (task != null && !task.isCompleted)
+                {
+                    taskManager.CompleteTask(task);
+                }
+            }
         }
+    }
+
+    Task FindTaskByName(string name)
+    {
+        foreach (Task task in taskManager.tasks)
+        {
+            if (task.taskName == name)
+                return task;
+        }
+        return null;
     }
 }
