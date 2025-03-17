75d09968f8916944a550bd6faf978aaf6924baef
daffaalqmr <daffaalqomaro123@gmail.com>
2025-03-18 02:05:14 +0700

    Update pickup item and fix super jump bug
    Update pickup item and fix super jump bug

--- Assets/Scripts/Mechanics/Interactions/Item.cs
+++ Assets/Scripts/Mechanics/Interactions/Item.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
+using System.Collections;
 
 public class Item : Interactable
 {
     [SerializeField] Transform playerHand;
     private Rigidbody rb;
+    private Collider itemCollider;
 
     private bool isHeld = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        itemCollider = GetComponent<Collider>();
     }
 
     public override string Description()
@@ -26,18 +29,39 @@ public class Item : Interactable
 
     void PickUp()
     {
-        transform.SetParent(playerHand);
-        transform.localPosition = Vector3.zero;
-        transform.localRotation = Quaternion.identity;
         rb.isKinematic = true;
+        rb.useGravity = false;
+        itemCollider.enabled = false;
         isHeld = true;
         PlayerInteractions.heldItem = this;
+
+        StartCoroutine(MoveToHand());
+    }
+
+    IEnumerator MoveToHand()
+    {
+        float duration = 0.2f;
+        float elapsed = 0f;
+        Vector3 startPos = transform.position;
+        Quaternion startRot = transform.rotation;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            transform.position = Vector3.Lerp(startPos, playerHand.position, elapsed / duration);
+            transform.rotation = Quaternion.Lerp(startRot, playerHand.rotation, elapsed / duration);
+            yield return null;
+        }
+
+        transform.SetParent(playerHand);
     }
 
     public void Drop()
     {
         transform.SetParent(null);
         rb.isKinematic = false;
+        rb.useGravity = true;
+        itemCollider.enabled = true;
         isHeld = false;
         PlayerInteractions.heldItem = null;
     }
