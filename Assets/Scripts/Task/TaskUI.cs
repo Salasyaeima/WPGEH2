e3a09f667666788c847157c65540c47415ca0c94
Deo silaen <deokeldisilaen@gmail.com>
2025-04-10 12:08:01 +0700

    update task

--- Assets/Scripts/Task/TaskUI.cs
+++ Assets/Scripts/Task/TaskUI.cs
@@ -5,18 +5,14 @@ using UnityEngine.UI;
 public class TaskUI : MonoBehaviour
 {
     public TMP_Text taskText;
-    public Image checkIcon;
     public Task task;
     public ITaskProvider taskProvider; // Cuma pake ini
 
     RectTransform textRectTransform;
-    RectTransform iconRectTransform;
 
     void Awake()
     {
         textRectTransform = taskText.GetComponent<RectTransform>();
-        if (checkIcon != null) iconRectTransform = checkIcon.GetComponent<RectTransform>();
-        if (checkIcon != null) checkIcon.enabled = false;
     }
 
     public void Initialize(Task task, ITaskProvider provider = null)
@@ -28,13 +24,6 @@ public class TaskUI : MonoBehaviour
         if (task.isCompleted)
         {
             StrikeThroughText();
-        }
-        else
-        {
-            if (checkIcon != null)
-            {
-                checkIcon.enabled = false;
-            }
         }
     }
 
@@ -55,25 +44,14 @@ public class TaskUI : MonoBehaviour
     {
         taskText.color = Color.gray;
         taskText.fontStyle = FontStyles.Strikethrough;
-        if (checkIcon != null)
-        {
-            checkIcon.enabled = true;
-        }
     }
 
     public void UpdateCheckIconPosition()
     {
-        if (checkIcon == null || textRectTransform == null || iconRectTransform == null)
-            return;
-
         float textWidth = taskText.preferredWidth;
         Vector2 textSize = textRectTransform.sizeDelta;
         textSize.x = textWidth;
         textRectTransform.sizeDelta = textSize;
-
-        float iconOffset = 10f;
-        iconRectTransform.anchoredPosition = new Vector2(textWidth + iconOffset, 0f);
-        iconRectTransform.sizeDelta = new Vector2(20f, 20f);
     }
 
     void Update()
