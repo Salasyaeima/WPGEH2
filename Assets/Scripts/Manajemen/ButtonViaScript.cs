1628441b77b4ee5803b90a9bceb6e13d761b2df5
daffaalqmr <daffaalqomaro123@gmail.com>
2025-06-11 06:52:44 +0700

    TWa

--- Assets/Scripts/Manajemen/ButtonViaScript.cs
+++ Assets/Scripts/Manajemen/ButtonViaScript.cs
@@ -1,21 +1,44 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ButtonViaScript : MonoBehaviour
 {
     [SerializeField] string GoToScene;
+    Button tombol;
+    LoadingScreen loadingScreen;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Button tombol = GetComponent<Button>();
-        LoadingScreen loadingScreen = GameObject.Find("LoadingScreenManager").GetComponent<LoadingScreen>();
-
-        tombol.onClick.AddListener(() => loadingScreen.SwitchToScene(GoToScene));
+        Debug.Log("Start dipanggil!");
+        StartCoroutine(insertListener());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator insertListener()
     {
-        
+        yield return new WaitForSeconds(1.5f);
+        tombol = GetComponent<Button>();
+        if (tombol == null)
+        {
+            Debug.LogError("Tombol tidak ditemukan di GameObject ini!");
+            yield break;
+        }
+
+        GameObject loadingScreenObj = GameObject.Find("LoadingScreenManager");
+        if (loadingScreenObj == null)
+        {
+            Debug.LogError("LoadingScreenManager tidak ditemukan!");
+            yield break;
+        }
+
+        loadingScreen = loadingScreenObj.GetComponent<LoadingScreen>();
+        if (loadingScreen == null)
+        {
+            Debug.LogError("Komponen LoadingScreen tidak ditemukan di LoadingScreenManager!");
+            yield break;
+        }
+
+        tombol.onClick.AddListener(() => loadingScreen.SwitchToScene(GoToScene));
+        Debug.Log("Listener berhasil ditambahkan!");
     }
 }
