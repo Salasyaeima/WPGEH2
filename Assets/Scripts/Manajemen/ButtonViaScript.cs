95ffa3460a56d5315b89df387d201f613e239ac9
daffaalqmr <daffaalqomaro123@gmail.com>
2025-06-11 08:00:22 +0700

    Update

--- Assets/Scripts/Manajemen/ButtonViaScript.cs
+++ Assets/Scripts/Manajemen/ButtonViaScript.cs
@@ -3,19 +3,17 @@ using UnityEngine.UI;
 
 public class ButtonViaScript : MonoBehaviour
 {
-    [SerializeField] string GoToScene;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
+    {
+        
+    }
+
+    public void addListenerButton(string GoToScene)
     {
         Button tombol = GetComponent<Button>();
         LoadingScreen loadingScreen = GameObject.Find("LoadingScreenManager").GetComponent<LoadingScreen>();
 
         tombol.onClick.AddListener(() => loadingScreen.SwitchToScene(GoToScene));
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
